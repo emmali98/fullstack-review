a30192ea17fc7dbb7194b960294788e4def4464d
Emma Li <emma.m.li.98@gmail.com>
2020-09-14 19:56:16 -0700

    Implements save method for database

--- database/index.js
+++ database/index.js
@@ -11,10 +11,20 @@ let repoSchema = mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (repos) => {
+  var reposToSave = repos.map((repo) => {
+    var rank = repo.watchers + repo.stargazers_count;
+    rank += repo.fork ? repo.forks : 0;
+    return {
+      name: repo.full_name,
+      owner: repo.owner.login,
+      url: repo.html_url,
+      rank: rank,
+      description: repo.description
+    };
+  });
+
+  return Repo.insertMany(reposToSave); // <-- a promise?
 }
 
 module.exports.save = save;
